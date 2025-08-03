32ce294405af240daae47f1bde0b5b6b5b9a0447
Ubuntu <ubuntu@ip-172-31-21-96.ap-southeast-2.compute.internal>
2025-08-03 10:45:03 +0000

    DateTimeDemo.java

---
+++ DateTimeDemo.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + now);
+    }
+}
+
